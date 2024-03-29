126d9f9d1a02a3449c73e87f8a07ade4773fdfd9
Anastasia Kravchenko <anastasia.kravchenko@sibers.com>
2024-03-29 16:26:11 +0700

    refactored view

--- DisneyApp/Flows/CharactersList/Views/CharactersListView.swift
+++ DisneyApp/Flows/CharactersList/Views/CharactersListView.swift
@@ -22,40 +22,109 @@ struct CharactersListView: View {
             case let .loaded(data):
                 ScrollView {
                     StickyHeader {
-                        VStack {
-                            if let url = URL(string: data.user.image) {
-                                WebImage(url: url)
-                                    .resizable()
-                                    .frame(width: 100, height: 100)
-                                    .clipShape(Circle())
-                                Text("Hello, \(data.user.name)!")
-                            } else {
-                                Image("placeholder")
-                                    .resizable()
-                                    .scaledToFit()
-                                Text("Not Authorized")
-                            }
-                        }
+                        self.headerView(
+                            image: data.user.image,
+                            name: data.user.name
+                        )
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .padding(.top, Const.headerTopPadding)
                     }
-                    VStack {
-                        ForEach(data.characters, id: \.self) { item in
-                            HStack {
-                                WebImage(url: URL(string: item.image))
-                                    .resizable()
-                                    .frame(width: 80, height: 80)
-                                    .clipShape(Circle())
-                                Text(item.name)
-
-                                Spacer()
-                            }
-                        }
-                    }
-                    .padding(.horizontal)
+                    self.listView(items: data.characters)
+                    .padding()
+                    .background(
+                        RoundedRectangle(
+                            cornerRadius: Const.cornerRadius
+                        )
+                        .fill(Color.white)
+                        .shadow(radius: Const.shadowRadius)
+                    )
                 }
+                .ignoresSafeArea(edges: .bottom)
+                .background(
+                    Color.green.opacity(
+                        Const.backgroundOpacity
+                    )
+                    .ignoresSafeArea(edges: .top)
+                )
+                .scrollIndicators(.hidden)
+
             case .error:
                 EmptyView()
             }
         }
         .onAppear { viewStore.loadData() }
     }
+
+    @ViewBuilder
+    private func headerView(
+        image: String,
+        name: String
+    ) -> some View {
+        VStack {
+            if let url = URL(string: image) {
+                WebImage(url: url)
+                    .resizable()
+                    .frame(
+                        width: Const.avatarWidth,
+                        height: Const.avatarHeight
+                    )
+                    .clipShape(Circle())
+                Text(Const.greetingText.replacingOccurrences(
+                    of: Const.textPlaceholder,
+                    with: name
+                ))
+                .padding(.vertical)
+            } else {
+                Image(Const.placholderImageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(
+                        width: Const.placholderWidth,
+                        height: Const.placholderHeight
+                    )
+                    .clipped()
+                Text(Const.notAuthorized)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func listView(
+        items: [CharactersListViewStore.State.Data.Character]
+    ) -> some View {
+        VStack {
+            ForEach(items, id: \.self) { item in
+                HStack {
+                    WebImage(url: URL(string: item.image))
+                        .resizable()
+                        .frame(
+                            width: Const.imageWidth,
+                            height: Const.imageHeight
+                        )
+                        .clipShape(Circle())
+                    Text(item.name)
+
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+private enum Const {
+    static let avatarWidth: CGFloat = 120
+    static let avatarHeight: CGFloat = 120
+    static let placholderWidth: CGFloat = 300
+    static let placholderHeight: CGFloat = 150
+    static let headerTopPadding: CGFloat = 30
+    static let imageWidth: CGFloat = 80
+    static let imageHeight: CGFloat = 80
+    static let cornerRadius: CGFloat = 20
+    static let shadowRadius: CGFloat = 8
+    static let backgroundOpacity: CGFloat = 0.2
+    static let notAuthorized: String = "Not Authorized"
+    static let placholderImageName: String = "placeholder"
+    static let greetingText: String = "Hello, %@!"
+    static let textPlaceholder: String = "%@"
 }
